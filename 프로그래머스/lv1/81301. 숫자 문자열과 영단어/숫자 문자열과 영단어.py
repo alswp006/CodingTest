7e659b6c1a1b7ddfcf1a7de6a43e8741a82fddd6
alswp006 <102672547+alswp006@users.noreply.github.com>
2023-01-16 22:08:32 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,7 @@
+def solution(s):
+    arr=["zero","one","two","three","four","five","six","seven","eight","nine"]
+    answer = 0
+    for i in range(len(arr)):
+        s=s.replace(arr[i],str(i))
+    print(s)
+    return int(s)

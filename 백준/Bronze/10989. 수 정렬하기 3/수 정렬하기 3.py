72f7f890264568fda6979a2b708e827fc9e10020
alswp006 <102672547+alswp006@users.noreply.github.com>
2023-02-10 23:36:20 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 8676 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+answer=[0 for _ in range(10001)]
+for i in range(int(input())):
+    answer[int(input())]+=1
+for i in range(len(answer)):
+    for j in range(1,answer[i]+1):
+        print(i)

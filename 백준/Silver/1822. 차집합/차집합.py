9bb259978db7b6b5f20f4dfdd3cde44721e6c759
alswp006 <102672547+alswp006@users.noreply.github.com>
2023-02-11 16:40:35 +0900

    [Silver IV] Title: 차집합, Time: 688 ms, Memory: 150008 KB -BaekjoonHub

--- 백준/Silver/1822. 차집합/차집합.py
+++ 백준/Silver/1822. 차집합/차집합.py
@@ -5,7 +5,8 @@ input=sys.stdin.readline
 n,m=map(int,input().split())
 a=set(map(int,input().split()))
 b=set(map(int,input().split()))
-c=sorted(list(a-b))
+c=list(a-b)
+c.sort()
 if len(c)==0:
     print(0)
 else:

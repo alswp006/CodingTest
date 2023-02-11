057cc36a722f1a4d81679f99b4482970c3950042
alswp006 <102672547+alswp006@users.noreply.github.com>
2023-02-11 16:19:45 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 556 ms, Memory: 165532 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -7,8 +7,10 @@ n= input()
 num= [i for i in input().split()]
 m= input()
 num2=[i for i in input().split()]
-num.sort()   
-for i in num2:
-    ri=bisect_right(num,i)
-    le=bisect_left(num,i)
-    print(ri-le,end=' ')
+dict={}
+for i in num:
+    if i in dict:
+        dict[i]+=1
+    else:
+        dict[i]=1
+print(' '.join(str(dict[i]) if i in dict else "0" for i in num2))

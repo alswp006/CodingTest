db1deb57d80344042bf94a1432adb8a4ca903a0c
alswp006 <102672547+alswp006@users.noreply.github.com>
2023-02-11 15:43:45 +0900

    [Silver IV] Title: 수 찾기, Time: 152 ms, Memory: 50412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,21 +1,8 @@
 import sys
-input=sys.stdin.readline
 
-def binary_search(array,target,start,end):
-    while start<=end:
-        mid=(start+end)//2
-        if array[mid]==target:
-            return 1
-        elif array[mid]>target:
-            end=mid-1
-        else:
-            start=mid+1
-    return 0
-
-n=int(input())
-number=list(map(int,input().split()))
-m=int(input())
-number2=list(map(int,input().split()))
-number.sort()
-for i in number2:
-    sys.stdout.write(str(binary_search(number,i,0,n-1))+'\n')
+n= input()
+num= set(input().split())
+m= input()
+num2=[i for i in input().split()]
+for i in num2:
+    print('1' if i in num else '0' )

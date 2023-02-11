78c8e2dcb5141a9c8df30b4f7576cc71f20abef8
alswp006 <102672547+alswp006@users.noreply.github.com>
2023-02-11 15:47:35 +0900

    [Silver V] Title: 숫자 카드, Time: 384 ms, Memory: 121544 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,21 +1,4 @@
-import sys
-
-input=sys.stdin.readline
-
-def b_s(arr,target,start,end):
-    while start<=end:
-        mid=(start+end)//2
-        if arr[mid]==target:
-            return "1"
-        elif arr[mid]<target:
-            start=mid+1
-        else:
-            end=mid-1
-    return "0"
-n=int(input())
-num=list(map(int,input().split()))
-m=int(input())
-num2=list(map(int,input().split()))
-num.sort()
-for i in num2:
-    sys.stdout.write(b_s(num,i,0,len(num)-1)+' ')
+n= input()
+num= set(input().split())
+m= input()
+print(' '.join(['1' if i in num else '0' for i in input().split()]))

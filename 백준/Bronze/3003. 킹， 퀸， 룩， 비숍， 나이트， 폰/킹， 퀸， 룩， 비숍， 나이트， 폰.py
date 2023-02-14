e2fcf51022db2219a1caefade13b115ddb5d9ac4
alswp006 <102672547+alswp006@users.noreply.github.com>
2023-02-14 16:28:34 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -1,9 +1,6 @@
 arr=list(map(int,input().split()))
 answer=[0 for i in range(len(arr))]
-answer[0]=1-arr[0]
-answer[1]=1-arr[1]
-answer[2]=2-arr[2]
-answer[3]=2-arr[3]
-answer[4]=2-arr[4]
-answer[5]=8-arr[5]
+c=[1,1,2,2,2,8]
+for i in range(6):
+    answer[i]=c[i]-arr[i]
 print(' '.join(map(str,answer)))

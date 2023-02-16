f91dbb2a586c1c3788c10cf2b2099196d6cbbcc6
alswp006 <102672547+alswp006@users.noreply.github.com>
2023-02-16 16:49:37 +0900

    [Silver II] Title: 에디터, Time: 320 ms, Memory: 46720 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -0,0 +1,18 @@
+import sys
+from collections import deque
+
+input=sys.stdin.readline
+
+left=deque(input().rstrip())
+right=deque()
+for _ in range(int(input())):
+    arr=input().rstrip().split()
+    if arr[0]=='L' and len(left)!=0:
+        right.appendleft(left.pop())
+    elif arr[0]=='D' and len(right)!=0:
+        left.append(right.popleft())
+    elif arr[0]=='B' and len(left)!=0:
+        left.pop()
+    elif arr[0]=='P':
+        left.append(arr[1])
+sys.stdout.write(''.join(left+right))

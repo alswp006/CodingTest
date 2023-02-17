764a8b4258eea7010b179d0d586816a8a546d99f
alswp006 <102672547+alswp006@users.noreply.github.com>
2023-02-17 15:14:24 +0900

    [Silver III] Title: 바이러스, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,22 +1,18 @@
-from collections import deque
 import sys
 
 input=sys.stdin.readline
 n=int(input())
 m=int(input())
 graph=[[] for i in range(n+1)]
-answer=0
 for _ in range(m):
     a,b=map(int,input().split())
     graph[a].append(b)
     graph[b].append(a)
-def dfs(graph,v,visited):
-    global answer
-    visited[v]=True
+def dfs(v):
+    visited[v]=1
     for i in graph[v]:
-        if not visited[i]:
-            answer+=1
-            dfs(graph,i,visited)
-visited=[False]*(n+1)
-dfs(graph,1,visited)
-print(answer)
+        if visited[i]==0:
+            dfs(i)
+visited=[0]*(n+1)
+dfs(1)
+print(sum(visited)-1)

9328ad376ae922bee58f47bb1e9633180f2aed0e
alswp006 <102672547+alswp006@users.noreply.github.com>
2023-02-17 18:01:00 +0900

    [Bronze III] Title: 알람 시계, Time: 236 ms, Memory: 18512 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		if(a>0) {
+		if(b<45) {
+			System.out.println((a-1) + " " + (b+15));
+		}
+		else if(b>=45)
+			System.out.println(a + " " + (b-45));
+		}
+		else {
+			if(b<45) {
+				System.out.println("23 " + (b+15));
+			}
+			else if(b>=45)
+				System.out.println(a + " " + (b-45));
+			}			
+	}
+}

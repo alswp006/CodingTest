b9b18780970f35a71687c155fc9613fd4f009033
alswp006 <102672547+alswp006@users.noreply.github.com>
2023-02-20 14:51:56 +0900

    [Silver I] Title: 숨바꼭질, Time: 232 ms, Memory: 40016 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+n,m=map(int,input().split())
+d=[0 for _ in range(200000)]
+
+q=deque()
+q.append(n)
+while q:
+    x=q.popleft()
+    if x==m:break
+    dx=[x-1,x+1,x*2]
+    for i in dx:
+        if 0<=i<200000 and not d[i]:
+            d[i]=d[x]+1
+            q.append(i)
+print(d[m])

fd6023ab49d5816b77081ea902bc5a74ac3d0e33
alswp006 <102672547+alswp006@users.noreply.github.com>
2023-02-20 15:48:21 +0900

    [level 2] Title: 전화번호 목록, Time: 171.89 ms, Memory: 28 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -2,6 +2,6 @@ def solution(phone_book):
     if len(phone_book)==1:return True
     phone_book.sort()
     for i in range(len(phone_book)-1):
-        if phone_book[i] in phone_book[i+1][:len(phone_book[i])]:
+        if phone_book[i+1].find(phone_book[i],0,len(phone_book[i]))==False:
             return False
     return True

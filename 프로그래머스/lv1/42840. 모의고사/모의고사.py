543fb1f5f754315726133be1972cc69cead27fa8
alswp006 <102672547+alswp006@users.noreply.github.com>
2023-02-22 21:27:53 +0900

    [level 1] Title: 모의고사, Time: 2.36 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -1,14 +1,14 @@
 def solution(answers):
-    answer = []
     arr=[0,0,0]
-    n1=[1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
-    n2=[2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5]
-    n3=[3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    n1=[1, 2, 3, 4, 5]
+    n2=[2, 1, 2, 3, 2, 4, 2, 5]
+    n3=[3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     for i in range(len(answers)):
-        if n1[i%len(n1)]==answers[i] : arr[0]+=1
-        if n2[i%len(n2)]==answers[i] : arr[1]+=1
-        if n3[i%len(n3)]==answers[i] : arr[2]+=1
-    for i in range(0,3):
-        if arr[i]==max(arr) : 
+        if answers[i]==n1[i%5]:arr[0]+=1
+        if answers[i]==n2[i%8]:arr[1]+=1
+        if answers[i]==n3[i%10]:arr[2]+=1
+    answer=[]
+    for i in range(len(arr)):
+        if arr[i]==max(arr):
             answer.append(i+1)
     return answer

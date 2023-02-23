ac842a42fcdd5959c1f8ee4a733e829689789705
alswp006 <102672547+alswp006@users.noreply.github.com>
2023-02-23 17:58:52 +0900

    [Silver IV] Title: 수 찾기, Time: 144 ms, Memory: 50408 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,8 +1,9 @@
 import sys
+import sys
+input = sys.stdin.readline
+n=input()
+arr=set(input().split())
+m=input()
+arr2=['1' if i in arr else '0' for i in input().split()]
 
-n= input()
-num= set(input().split())
-m= input()
-num2=[i for i in input().split()]
-for i in num2:
-    print('1' if i in num else '0' )
+for i in arr2:print(i)

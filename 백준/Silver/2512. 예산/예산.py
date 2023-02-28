0152b2186293630390ba92d7fa77508f674cc8f7
alswp006 <102672547+alswp006@users.noreply.github.com>
2023-02-28 20:25:04 +0900

    [Silver III] Title: 예산, Time: 44 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.py
+++ 백준/Silver/2512. 예산/예산.py
@@ -1,21 +1,17 @@
 import sys
 
 input=sys.stdin.readline
-
-n=int(input())
+n=int(input().rstrip())
 arr=list(map(int,input().split()))
-m=int(input())
-if sum(arr)<m:
-    print(max(arr))
-else:
-    while arr:
-        x=m//n
-        for i in arr:
-            if i<x:
-                m-=i
-                n-=1
-                arr.remove(i)
-                break
-        else:
-            break    
-    print(m//n)
+money=int(input().rstrip())
+arr.sort()
+num=0
+for i in range(n):
+    if money//(n-i)>=arr[i]:
+        money-=arr[i]
+    else:
+        money=money//(n-i)
+        break
+    num+=1
+
+print(money if money<=arr[-1] and num!=n else arr[-1])

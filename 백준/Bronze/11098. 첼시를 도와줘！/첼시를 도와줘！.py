2f93dfbdd34e0eacf92168fd5996e987eb0c92c3
alswp006 <102672547+alswp006@users.noreply.github.com>
2023-03-21 09:42:56 +0900

    [Bronze II] Title: 첼시를 도와줘!, Time: 276 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/11098. 첼시를 도와줘！/첼시를 도와줘！.py
@@ -0,0 +1,6 @@
+for _ in range(int(input())):
+    arr=[]
+    for i in range(int(input())):
+        arr.append(list(map(str,input().split())))
+    arr.sort(key=lambda x : int(x[0]))
+    print(arr[-1][-1])

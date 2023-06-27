0612fea7cb5763313f7543c633684efd2dd46f6f
alswp006 <102672547+alswp006@users.noreply.github.com>
2023-06-27 20:42:56 +0900

    [Bronze II] Title: 화성 수학, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/5355. 화성 수학/화성 수학.py
+++ 백준/Bronze/5355. 화성 수학/화성 수학.py
@@ -1,11 +1,8 @@
-n=int(input())
-arr=[]
-for i in range(n):
-    arr.append(list(map(str,input().split())))
-for i in range(n):
-    arr[i][0]=float(arr[i][0])
-    for j in range(1,len(arr[i])):
-        if arr[i][j]=='@':arr[i][0]*=3
-        elif arr[i][j]=='%':arr[i][0]+=5
-        elif arr[i][j]=='#':arr[i][0]-=7
-    print("{:.2f}".format(arr[i][0]))
+for i in range(int(input())):
+    arr=list(map(str,input().split()))
+    arr[0]=float(arr[0])
+    for j in range(1,len(arr)):
+        if arr[j]=='@': arr[0]*=3
+        elif arr[j]=='%': arr[0]+=5
+        elif arr[j]=='#': arr[0]-=7
+    print("{:.2f}".format(arr[0]))

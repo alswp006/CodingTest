6972393db552f964ae9f07501c24e7c054ddf5ea
alswp006 <102672547+alswp006@users.noreply.github.com>
2023-06-27 20:28:29 +0900

    [Bronze IV] Title: 인공지능 시계, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2530. 인공지능 시계/인공지능 시계.py
+++ 백준/Bronze/2530. 인공지능 시계/인공지능 시계.py
@@ -1,12 +1,10 @@
 a,b,c=map(int,input().split())
-d=int(input())
-c+=d
+c+=(int(input()))
 if c>=60:
     b+=c//60
-    c%=60
+    c=c%60
 if b>=60:
     a+=b//60
-    b%=60
-if a>=24:
-    a%=24
-print("{0} {1} {2}".format(a,b,c))
+    b=b%60
+if a>=24: a=a%24
+print(a,b,c)

e20c9819593c3938f38a4a7d9cfcccef9724a93e
alswp006 <102672547+alswp006@users.noreply.github.com>
2023-07-04 10:31:10 +0900

    [Silver IV] Title: 보물, Time: 68 ms, Memory: 34272 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.py
+++ 백준/Silver/1026. 보물/보물.py
@@ -1,13 +1,17 @@
+import sys
 from collections import deque
-n=int(input())
-a=list(map(int,input().split()))
-b=list(map(int,input().split()))
-a.sort()
-a=deque(a)
-b=deque(b)
-answer=0
-for i in range(len(b)):
-    min=a.popleft()
-    answer+=min*max(b)
-    b.remove(max(b))
-print(answer)
+def main():
+    n=int(input())
+    a=list(map(int,input().split()))
+    b=list(map(int,input().split()))
+    a.sort()
+    a=deque(a)
+    b=deque(b)
+    answer=0
+    for i in range(len(b)):
+        min=a.popleft()
+        answer+=min*max(b)
+        b.remove(max(b))
+    print(answer)
+if __name__ == "__main__":
+    sys.exit(main())

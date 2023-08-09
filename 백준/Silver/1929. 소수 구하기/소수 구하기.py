2bd94e733342cb207230c0bbe8331aed64b570bf
alswp006 <102672547+alswp006@users.noreply.github.com>
2023-08-09 15:25:11 +0900

    [Silver III] Title: 소수 구하기, Time: 760 ms, Memory: 38928 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,8 +1,8 @@
 n,m=map(int,input().split())
-arr=[0]*(m+1)
-arr[1]=1
+arr=[True]*(m+1)
+arr[1]=False
 for i in range(2,int(m**0.5)+1):
     for j in range(2,m//i+1):
-        arr[i*j]=1
+        arr[i*j]=False
 for i in range(n,m+1):
-    if arr[i]==0:print(i)
+    if arr[i]:print(i)

3660f92c88d76f704ca97d4bc090209067a315c8
alswp006 <102672547+alswp006@users.noreply.github.com>
2023-08-29 16:33:26 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1072 ms, Memory: 73176 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -0,0 +1,16 @@
+import sys
+from collections import deque 
+
+input = sys.stdin.readline
+
+n,m = map(int,input().split())
+arr=[[0 for _ in range(n+1)] for _ in range(n+1)]
+for i in range(1,n+1):
+    temp = deque(map(int,input().split()))
+    temp.appendleft(0)
+    for j in range(1,n+1):
+        arr[i][j]=arr[i-1][j]+arr[i][j-1]-arr[i-1][j-1]+temp[j]
+#합 배열 만들기
+for _ in range(m):
+    x1,y1,x2,y2=map(int,input().split())
+    print(arr[x2][y2]-arr[x1-1][y2]-arr[x2][y1-1]+arr[x1-1][y1-1])

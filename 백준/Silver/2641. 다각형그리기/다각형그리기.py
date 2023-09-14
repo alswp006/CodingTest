c79652751a0b993bb9b83a9efcdf60b221393f6c
alswp006 <102672547+alswp006@users.noreply.github.com>
2023-09-14 20:17:21 +0900

    [Silver II] Title: 다각형그리기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2641. 다각형그리기/다각형그리기.py
+++ 백준/Silver/2641. 다각형그리기/다각형그리기.py
@@ -1,32 +1,35 @@
 import sys
 
 input = sys.stdin.readline
-length = int(input())#표본수열의 길이를 받습니다.
 
-stand = ' '.join(input().split())#기준이 될 표본수열을 받습니다.나중에 비교해줍니다.
-copy = ''#반대방향으로 돌리는 경우를 아래 반복문으로 만들어줍니다.
+length = int(input())
+stand = list(map(str,input().split()))
+reverse = []
 
-for i in range(len(stand)-1,-1,-1):#회전방향과 상하좌우를 동시에 바꿔줍니다.
-    if stand[i] == ' ':
-        copy += ' '
-    elif stand[i] == '1':
-        copy += '3'
+for i in range(length-1,-1,-1):#회전방향과 상하좌우를 동시에 바꿔줍니다.
+    if stand[i] == '1':
+        reverse.append('3')
     elif stand[i] == '2':
-        copy += '4'
+        reverse.append('4')
     elif stand[i] == '3':
-        copy += '1'
+        reverse.append('1')
     elif stand[i] == '4':
-        copy += '2'
-        
-stand = stand + ' ' + stand
-copy = copy + ' ' + copy
-#회전을 하는 경우이기 때문에 시작과 끝을 이어주는 느낌으로 표본수열을 늘려줍니다.
-to_for = int(input())#본격적으로 비교할 모양수열들의 갯수
-total = []#출력할 모양수열을 저장하는 리스트
-for i in range(to_for):
-    comp = ' '.join(input().split())#모양수열을 받습니다.
-    if comp in stand or comp in copy:#모양수열을 두가지 회전방향으로 검사합니다.
-        total.append(comp)#맞다면 total리스트에 추가합니다.
-print(len(total))#원소의 갯수 출력
-for ans in total:
-    print(ans)#원소 출력 형식에 맞게 줄바꿔서 출력
+        reverse.append('2')
+
+#표본 수열 두 번 반복하기
+temp=stand+stand
+stand = ' '.join(temp)
+temp=reverse+reverse
+reverse = ' '.join(temp)
+
+#모양 수열 비교
+answer = []
+for _ in range(int(input())):
+    comp = ' '.join(input().split())
+    if comp in stand or comp in reverse:
+        answer.append(comp)
+
+#정답 출력
+print(len(answer))
+for ans in answer:
+    print(ans)

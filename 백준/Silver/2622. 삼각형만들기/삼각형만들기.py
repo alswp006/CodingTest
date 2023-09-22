560d29e7e473e37c6cf0b7c8b2cb97f791eedfdd
alswp006 <102672547+alswp006@users.noreply.github.com>
2023-09-22 19:58:35 +0900

    [Silver V] Title: 삼각형만들기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2622. 삼각형만들기/삼각형만들기.py
+++ 백준/Silver/2622. 삼각형만들기/삼각형만들기.py
@@ -1,20 +1,7 @@
 n=int(input())
 answer=0
-largestSide=0
 
-if n%2==0:
-    largestSide = n // 2 - 1
-else:
-    largestSide=n // 2
-for i in range(largestSide,n//3,-1):
-    m=0
-    big_side = i-m
-    small_side = n - i - big_side
-    while big_side>=small_side:
-        answer+=1
-        m+=1
-        big_side=i-m
-        small_side=n-i-big_side
-if n%3==0:
-    answer+=1
+for i in range((n+1)//3,(n+1)//2):
+    small_side = n - (i*2)
+    answer+=(i-small_side)//2+1
 print(answer)

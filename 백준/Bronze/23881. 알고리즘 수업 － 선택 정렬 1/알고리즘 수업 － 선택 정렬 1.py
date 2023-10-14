1e1555744c3a215c8e51c13bf11745e52111cbcb
alswp006 <102672547+alswp006@users.noreply.github.com>
2023-10-14 11:50:08 +0900

    [Bronze I] Title: 알고리즘 수업 - 선택 정렬 1, Time: 208 ms, Memory: 110712 KB -BaekjoonHub

--- 백준/Bronze/23881. 알고리즘 수업 － 선택 정렬 1/알고리즘 수업 － 선택 정렬 1.py
+++ 백준/Bronze/23881. 알고리즘 수업 － 선택 정렬 1/알고리즘 수업 － 선택 정렬 1.py
@@ -8,14 +8,11 @@ count=0
 while arr_sort:
     for i in range(len(arr)-1,-1,-1):
         if arr[i] == arr_sort[-1] :
-            if i == len(arr)-1:
-                arr.pop()
-                arr_sort.pop()
+            a, b = arr.pop(), arr_sort.pop()
+            if a==b:continue
             else:
                 count+=1
-                arr[i],arr[-1] = arr[-1],arr[i]
-                a,b=arr[i],arr_sort.pop()
-                arr.pop()
+                arr[i]=a
                 if count==k:
                     print(a,b)
                     exit()

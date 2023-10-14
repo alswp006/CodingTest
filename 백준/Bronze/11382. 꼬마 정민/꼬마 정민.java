c1260cdb3f1ff5dbe4eaf3d88cc6b39b03cc5c73
alswp006 <102672547+alswp006@users.noreply.github.com>
2023-10-15 01:21:16 +0900

    [Bronze V] Title: 꼬마 정민, Time: 124 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -1,11 +1,14 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        long n1 = sc.nextLong();
-        long n2 = sc.nextLong();
-        long n3 = sc.nextLong();
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long n1 = Long.parseLong(st.nextToken());
+        long n2 = Long.parseLong(st.nextToken());
+        long n3 = Long.parseLong(st.nextToken());
 
         System.out.println(n1+n2+n3);
     }

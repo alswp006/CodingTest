162e37746c021b72934b49feb408b8244000c312
alswp006 <102672547+alswp006@users.noreply.github.com>
2023-10-19 00:24:03 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 76 ms, Memory: 11528 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -4,14 +4,13 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        String[] str_arr = str.split("");
+        char[] str = br.readLine().toCharArray();
         int[] answer_arr = new int[26];
         StringBuilder answer = new StringBuilder();
         Arrays.fill(answer_arr,-1);
 
-        for (int i = 0; i<str_arr.length; i++){
-            int ascii_alphabet = (int)str_arr[i].charAt(0)-97;
+        for (int i = 0; i<str.length; i++){
+            int ascii_alphabet = (int)str[i]-97;
             if (answer_arr[ascii_alphabet]==-1){
                 answer_arr[ascii_alphabet] = i;
             }

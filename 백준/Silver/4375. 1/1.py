df59a3807aa0ddab6655631be923939167588e0c
alswp006 <102672547+alswp006@users.noreply.github.com>
2023-11-26 23:01:16 +0900

    [Silver III] Title: 1, Time: 164 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.py
+++ 백준/Silver/4375. 1/1.py
@@ -3,8 +3,10 @@ while True:
         n = int(input())
     except Exception:
         break
+    i = 1
     num = 1
 
     while num % n != 0:
         num = num * 10 + 1
-    print(len(str(num)))
+        i += 1
+    print(i)

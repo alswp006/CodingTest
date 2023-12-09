2ac95c9b33622e3885fd838fbb6c160fb48da68c
alswp006 <102672547+alswp006@users.noreply.github.com>
2023-12-10 01:14:35 +0900

    [Silver IV] Title: 나이트 투어, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1331. 나이트 투어/나이트 투어.py
+++ 백준/Silver/1331. 나이트 투어/나이트 투어.py
@@ -3,12 +3,8 @@ def check_knight():
     for i in range(len(arr)):
         alp = abs(ord(arr[i - 1][0]) - ord(arr[i][0]))
         num = abs(int(arr[i - 1][1]) - int(arr[i][1]))
-
         if not ((alp == 2 and num == 1) or (alp == 1 and num == 2)) :
             return False
     return True
-
-
 arr = [sys.stdin.readline().rstrip() for i in range(36)]
-
 print("Invalid" if len(set(arr)) != 36 or check_knight() == False else "Valid")

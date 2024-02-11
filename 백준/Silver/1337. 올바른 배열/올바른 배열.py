6ef88ba44d98ee528e29d0824c0727172e835c82
alswp006 <102672547+alswp006@users.noreply.github.com>
2024-02-11 23:16:34 +0900

    [Silver IV] Title: 올바른 배열, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1337. 올바른 배열/올바른 배열.py
+++ 백준/Silver/1337. 올바른 배열/올바른 배열.py
@@ -2,12 +2,12 @@ import sys
 
 input = sys.stdin.readline
 arr = sorted(int(input()) for i in range(int(input())))
-answer = 10**9
+answer = 4
 
 for i in arr:
-    count = 0
-    for j in range(i,i+5):
-        if j not in arr:
-            count+=1
+    count = 4
+    for j in range(i+1,i+5):
+        if j in arr:
+            count-=1
     answer = min(answer,count)
 print(answer)

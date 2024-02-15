6a8e2570ec61a6ae83465a0a88fc3cbb133a27a1
alswp006 <102672547+alswp006@users.noreply.github.com>
2024-02-16 00:21:22 +0900

    [Silver III] Title: 1로 만들기, Time: 532 ms, Memory: 38932 KB -BaekjoonHub

---
+++ 백준/Silver III/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,10 @@
+arr= [0] * (int(input()) + 1)
+
+for i in range(2, len(arr)):
+    arr[i]=arr[i-1]+1
+    if i%2==0:
+        arr[i]=min(arr[i],arr[i//2]+1)
+    if i%3==0:
+        arr[i]=min(arr[i],arr[i//3]+1)
+
+print(arr[-1])

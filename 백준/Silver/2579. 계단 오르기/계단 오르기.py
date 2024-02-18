17710fb9c5ab05ca61446711e10fae074518c9f4
alswp006 <102672547+alswp006@users.noreply.github.com>
2024-02-18 13:23:32 +0900

    [Silver III] Title: 계단 오르기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,14 +1,19 @@
-n=int(input())
-answer=[0]*301
-arr=[0]
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+arr = [0]
 for _ in range(n):
     arr.append(int(input()))
-if len(arr)==2:answer[1]=arr[1]
-elif len(arr)==3:answer[2]=arr[1]+arr[2]
-else:
-    answer[1]=arr[1]
-    answer[2]=answer[1]+arr[2]
-    answer[3]=max(arr[1]+arr[3],arr[2]+arr[3])
-    for i in range(4,n+1):
-        answer[i]=max(answer[i-3]+arr[i-1]+arr[i],answer[i-2]+arr[i])
+answer = [0] * 301
+
+answer[1] = arr[1]
+if n > 1:
+    answer[2] = arr[1] + arr[2]
+if n > 2:
+    answer[3] = max(arr[1], arr[2]) + arr[3]
+if n > 3:
+    for i in range(4, n + 1):
+        answer[i] = max(answer[i - 3] + arr[i - 1], answer[i - 2]) + arr[i]
 print(answer[n])

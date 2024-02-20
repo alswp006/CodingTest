428b07eaecf9163effb17d75ece6c65506302efd
alswp006 <102672547+alswp006@users.noreply.github.com>
2024-02-20 18:35:03 +0900

    [Silver I] Title: RGB거리, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,17 +1,10 @@
 import sys
 input=sys.stdin.readline
 arr=[]
-answer=[]
 for i in range(int(input())):
     arr.append(list(map(int,input().split())))
-    answer.append([0 for i in range(3)])
-answer[0]=arr[0]
-for i in range(1,len(answer)):
-    for j in range(3):
-        if j==0:
-            answer[i][j]=min(answer[i-1][j+1],answer[i-1][j+2])+arr[i][j]
-        elif j==1:
-            answer[i][j]=min(answer[i-1][j-1],answer[i-1][j+1])+arr[i][j]
-        elif j==2:
-            answer[i][j]=min(answer[i-1][j-2],answer[i-1][j-1])+arr[i][j]
-print(min(answer[-1]))
+for i in range(1,len(arr)):
+    arr[i][0]=min(arr[i-1][1],arr[i-1][2])+arr[i][0]
+    arr[i][1]=min(arr[i-1][0],arr[i-1][2])+arr[i][1]
+    arr[i][2]=min(arr[i-1][0],arr[i-1][1])+arr[i][2]
+print(min(arr[-1]))

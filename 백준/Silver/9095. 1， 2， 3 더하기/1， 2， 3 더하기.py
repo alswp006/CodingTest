88f695dbaa2894308500c2499bd5ad16c41e4b90
alswp006 <102672547+alswp006@users.noreply.github.com>
2024-02-20 17:38:05 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,13 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
 for _ in range(int(input())):
     n = int(input())
     count = 0
-    def cal(n):
-        global count
-        if n == 0:
+    stack = [(n, 0)]  # 스택에 (숫자, 횟수) 형태로 저장합니다.
+
+    while stack:
+        num, cnt = stack.pop()
+
+        if num == 0:
             count += 1
-            return -1
-        if n<0: return -1
-        for i in range(1,4):
-            cal(n-i)
-    cal(n)
+            continue
+
+        for i in range(1, 4):
+            if num - i >= 0:
+                stack.append((num - i, cnt + 1))
+
     print(count)

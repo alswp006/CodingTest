aacea12bb9d2e4faec198c0e70c6e557bd27620b
alswp006 <102672547+alswp006@users.noreply.github.com>
2024-02-22 01:08:46 +0900

    [Gold V] Title: 동전 1, Time: 180 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.py
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -1,13 +1,12 @@
 import sys
 
 input = sys.stdin.readline
-
 n, k = map(int, input().split())
 arr = [int(input()) for i in range(n)]
-answer = [0] * (k + 1)
-answer[0] = 1
+answer = [1] + [0] * (k + 1)
 
 for i in arr:
     for j in range(i, k + 1):
         answer[j] += answer[j - i]
+
 print(answer[k])

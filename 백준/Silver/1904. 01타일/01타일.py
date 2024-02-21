3dd3f9801855160aeb2bfb5502f696691d263dc6
alswp006 <102672547+alswp006@users.noreply.github.com>
2024-02-22 00:05:49 +0900

    [Silver III] Title: 01타일, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -1,9 +1,17 @@
+# 피사노 주기 구하기
+# def pisano_period(m):
+#     previous, current = 0, 1
+#     for i in range(0, m * m):
+#         previous, current = current, (previous + current) % m
+#
+#         # 피사노 주기는 항상 0과 1로 시작합니다.
+#         if (previous == 0 and current == 1):
+#             return i + 1
+# m = 15746일 때 피사노 주기 47244
+a, b = 1,2
+
 n = int(input())
+for i in range(n % 47244 - 2):
+    a, b = b, (a+b) % 15746
 
-a = 1
-b = 2
-
-for i in range(3, n + 1):
-    a, b = b, (a+b)%15746
-
-print(b if n != 1 else 1)
+print(b if n != 1 else a)

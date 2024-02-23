f446c1979721c3e401855127de83208b3adad0a6
alswp006 <102672547+alswp006@users.noreply.github.com>
2024-02-23 20:52:54 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 76 ms, Memory: 31408 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
@@ -0,0 +1,42 @@
+import sys
+
+input = sys.stdin.readline
+
+a, b, c = 0, 0, 0
+answer = 0
+di = dict()
+di[(0, 0, 0)] = 1
+di[(20, 20, 20)] = 1048576
+
+
+def w(a, b, c):
+    if (a, b, c) in di.keys():
+        return di[(a, b, c)]
+
+    if a <= 0 or b <= 0 or c <= 0:
+        return di[(0, 0, 0)]
+
+    if a > 20 or b > 20 or c > 20:
+        return di[(20, 20, 20)]
+
+    if a < b and b < c:
+        di[(a, b, c)] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c)
+        return di[(a, b, c)]
+    else:
+        di[(a, b, c)] = (
+                w(a - 1, b, c)
+                + w(a - 1, b - 1, c)
+                + w(a - 1, b, c - 1)
+                - w(a - 1, b - 1, c - 1)
+        )
+        return di[(a, b, c)]
+
+
+while True:
+    a, b, c = map(int, input().split())
+    if a == b == c == -1:
+        break
+    if (a, b, c) in di.keys():
+        print("w(%d, %d, %d) = %d" % (a, b, c, di[(a, b, c)]))
+        continue
+    print("w(%d, %d, %d) = %d" % (a, b, c, w(a, b, c)))

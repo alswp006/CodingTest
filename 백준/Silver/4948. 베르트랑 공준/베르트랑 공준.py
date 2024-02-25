ba9e0ae91d7b70c308f9c19389992a4e4a83785b
alswp006 <102672547+alswp006@users.noreply.github.com>
2024-02-25 23:28:56 +0900

    [Silver II] Title: 베르트랑 공준, Time: 484 ms, Memory: 34980 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -2,14 +2,12 @@ import sys
 
 input = sys.stdin.readline
 
-d = [False for i in range(123457 * 2)]
-primes = set()
+d = [False, False] + [True] * (123457 * 2 + 1)
 
-for i in range(2, len(d)):
-  if not d[i]:
-    primes.add(i)
-    for j in range(2*i, len(d), i):
-        d[j] = True
+for i in range(2, int(len(d) ** 0.5)+1):
+    if d[i]:
+        for j in range(i * 2, len(d), i):
+            d[j] = False
 
 while True:
     n = int(input())
@@ -19,7 +17,7 @@ while True:
         break
 
     for i in range(n + 1, 2 * n + 1):
-        if i in primes:
+        if d[i]:
             count += 1
 
     print(count)

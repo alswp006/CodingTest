4a76276a35a1146b0c258eab88c031295e49f041
alswp006 <102672547+alswp006@users.noreply.github.com>
2024-02-26 17:14:28 +0900

    [level 2] Title: 숫자의 표현, Time: 0.98 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = 0
+    
+    for i in range(1, int(n**0.5) + 1):
+        if n % i == 0:
+            if i % 2 != 0:
+                answer+=1
+            if n//i % 2 != 0 and i != n//i:
+                answer+=1
+    
+        
+    return answer

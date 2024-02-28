7d026c425f3219aa8c6feaee985ae365b1404712
alswp006 <102672547+alswp006@users.noreply.github.com>
2024-02-29 01:34:04 +0900

    [level 4] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,12 +1,8 @@
-select c.car_id, c.car_type,round((c.daily_fee-(c.daily_fee*d.discount_rate/100))*30) FEE
-from car_rental_company_car c, (select car_type, discount_rate
-                               from car_rental_company_discount_plan
-                               where duration_type like "30일%"
-                               and car_type in ("SUV", "세단")) d
-WHERE c.car_type = d.car_type and c.car_id NOT IN (
-    SELECT car_id 
-    FROM car_rental_company_rental_history
-    where start_date<'2022-12-00' and end_date>'2022-11-00'
-)
-having FEE>=500000 and FEE<2000000
+select r.car_id, r.car_type, round(r.daily_fee * 30 *(100 - d.discount_rate)/100) FEE
+from CAR_RENTAL_COMPANY_CAR R, (select car_type, discount_rate
+                               from CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+                               where car_type in ("SUV","세단")
+                               and duration_type like "30일%") D
+where r.car_id not in (Select car_id from CAR_RENTAL_COMPANY_RENTAL_HISTORY where start_date<'2022-12-00' and end_date>'2022-11-00') and r.car_type = d.car_type
+having FEE >= 500000 and FEE < 2000000
 order by 3 desc, 2, 1 desc

250c8e33c6541f9cc9076b345cd05b4c084651ba
alswp006 <102672547+alswp006@users.noreply.github.com>
2024-03-01 00:33:35 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,8 @@
+select ID, EMAIL, FIRST_NAME, LAST_NAME
+from DEVELOPERS
+where skill_code & (select code from skillcodes 
+      where name = "C#")
+or 
+skill_code & (select code from skillcodes 
+      where name = "Python")
+order by ID

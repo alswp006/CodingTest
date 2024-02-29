b61242ad91cc2b8e62819166fff387fa0753ff47
alswp006 <102672547+alswp006@users.noreply.github.com>
2024-02-29 15:48:32 +0900

    [Silver II] Title: 숫자판 점프, Time: 68 ms, Memory: 34068 KB -BaekjoonHub

---
+++ 백준/Silver/2210. 숫자판 점프/숫자판 점프.py
@@ -0,0 +1,39 @@
+from collections import deque
+
+arr=[]
+answer = set()
+d = [(1,0), (-1,0), (0,1), (0,-1)]
+
+for i in range(5):
+    arr.append(list(map(str,input().split())))
+
+visited = [[False for i in range(5)] for j in range(5)]
+
+def init():
+    for i in range(5):
+        for j in range(5):
+            visited[i][j] = False
+
+def bfs(x1,y1):
+    q = deque()
+    q.append((x1,y1,0,""))
+    while q:
+        x,y,count,str = q.popleft()
+        visited[x][y] = True
+        str+=arr[x][y]
+        if count == 5:
+            answer.add(str)
+            continue
+        for i in range(4):
+            nx = x + d[i][0]
+            ny = y + d[i][1]
+            if nx < 0 or ny < 0 or nx >= 5 or ny >= 5:
+                continue
+            q.append((nx,ny,count+1,str))
+    return
+
+for i in range(5):
+    for j in range(5):
+        bfs(i,j)
+        init()
+print(len(answer))

8020957cc8d20581fbfeaa7fce8a137beb50d82e
alswp006 <102672547+alswp006@users.noreply.github.com>
2024-03-18 10:40:57 +0900

    [Gold IV] Title: 테트로미노, Time: 188 ms, Memory: 113516 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -1,49 +1,42 @@
 import sys
 input = sys.stdin.readline
 
-shape = [
-    [(0, 1), (1, 0), (1, 1)],
-    [(0, 1), (0, 2), (0, 3)],
-    [(1, 0), (2, 0), (3, 0)],
-    [(1, 0), (2, 0), (2, 1)],
-    [(1, 0), (2, 0), (2, -1)],
-    [(1, 0), (1, 1), (1, 2)],
-    [(0, 1), (1, 0), (2, 0)],
-    [(0, 1), (0, 2), (1, 0)],
-    [(0, 1), (0, 2), (-1, 2)],
-    [(0, 1), (1, 1), (2, 1)],
-    [(1, 0), (1, 1), (2, 1)],
-    [(0, 1), (0, 2), (1, 1)],
-    [(1, 0), (1, 1), (2, 0)],
-    [(0, 1), (0, 2), (-1, 1)],
-    [(1, 0), (1, -1), (2, 0)],
-    [(0, 1), (0, 2), (1, 2)],
-    [(1, 0), (1, -1), (2, -1)],
-    [(0, 1), (1, 1), (1, 2)],
-    [(0, 1), (1, 0), (1, -1)],
-    [(0, 1), (1, 1), (1, 0)],
-    [(0, 1), (1, 1), (-1, 1)]
-]
+N, M = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(N)]
+max_num = max(map(max, board))
 
-n, m = map(int, input().split())
-arr = [list(map(int, input().split())) for i in range(n)]
-
-def get_result(x, y):
-    global answer
-    for i in range(len(shape)):
-        result = arr[x][y]
-        for dx, dy in shape[i]:
-            if 0 <= x + dx < n and 0 <= y + dy < m:
-                result += arr[x + dx][y + dy]
-                continue
-            break
-        else:
-            answer = max(answer, result)
-
+d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 answer = 0
 
-for i in range(n):
-    for j in range(m):
-        get_result(i, j)
+def dfs(r, c, cnt, res):
+    global answer
+    if res + (4 - cnt) * max_num < answer:
+        return
+    
+    if cnt == 4:
+        answer = max(answer, res)
+        return
+    
+    for dr, dc in d:
+        nr, nc = r + dr, c + dc
+        if 0 <= nr < N and 0 <= nc < M and board[nr][nc] > 0:
+            temp = board[nr][nc]
+            if cnt == 2:
+                board[nr][nc] = 0
+                dfs(r, c, cnt + 1, res + temp)
+                board[nr][nc] = temp
+            
+            board[nr][nc] = 0
+            dfs(nr, nc, cnt + 1, res + temp)
+            board[nr][nc] = temp
+    
+def solution():
+    for i in range(N):
+        for j in range(M):
+            temp = board[i][j]
+            board[i][j] = 0
+            dfs(i, j, 1, temp)
+            board[i][j] = temp
+    return answer
 
-print(answer)
+print(solution())

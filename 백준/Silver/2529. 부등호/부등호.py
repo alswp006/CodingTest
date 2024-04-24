baa7cef5d6c1672b996d11e409b82280f9dfb90c
Minje Kim <102672547+alswp006@users.noreply.github.com>
2024-04-24 16:14:33 +0900

    [Silver I] Title: 부등호, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.py
+++ 백준/Silver/2529. 부등호/부등호.py
@@ -10,6 +10,9 @@ arr_min = arr
 if arr_max[0] == ">":
     answer_max.append(desc)
     desc -= 1
+else:
+    answer_min.append(asc)
+    asc += 1
 
 for i in range(n):
     len_under = len(arr_max) - len(arr_max.lstrip("<"))
@@ -20,37 +23,27 @@ for i in range(n):
             answer_max.append(i)
         arr_max = arr_max.lstrip("<")
         desc -= len_under + 1
+
+        for i in range(asc, asc + len_under - 1):
+            answer_min.append(i)
+        arr_min = arr_min.lstrip("<")
+        asc += len_under - 1
+
     if len_over > 0:
         for i in range(desc, desc - len_over + 1, -1):
             answer_max.append(i)
         arr_max = arr_max.lstrip(">")
         desc -= len_over - 1
-else:
-    if arr[-1] == ">":
-        answer_max.append(desc)
 
-if arr_min[0] == "<":
-    answer_min.append(asc)
-    asc += 1
-
-for i in range(n):
-    len_under = len(arr_min) - len(arr_min.lstrip("<"))
-    len_over = len(arr_min) - len(arr_min.lstrip(">"))
-
-    if len_under > 0:
-        for i in range(asc, asc + len_under - 1):
-            answer_min.append(i)
-        arr_min = arr_min.lstrip("<")
-        asc += len_under - 1
-    if len_over > 0:
         for i in range(asc + len_over, asc - 1, -1):
             answer_min.append(i)
         arr_min = arr_min.lstrip(">")
         asc += len_over + 1
 else:
-    if arr[-1] == "<":
+    if arr[-1] == ">":
+        answer_max.append(desc)
+    else:
         answer_min.append(asc)
-
 
 print(''.join(map(str, answer_max)))
 print(''.join(map(str, answer_min)))

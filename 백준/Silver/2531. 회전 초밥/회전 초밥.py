3286e79818e62c53f14c6081cc909c38ea03c385
Minje Kim <102672547+alswp006@users.noreply.github.com>
2024-04-26 15:08:25 +0900

    [Silver I] Title: 회전 초밥, Time: 2024 ms, Memory: 189740 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.py
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.py
@@ -2,7 +2,8 @@ import sys
 
 input = sys.stdin.readline
 n, d, k, c = map(int, input().split())
-arr = [int(input()) for i in range(n)] * 2
+arr = [int(input()) for i in range(n)]
+arr += arr[:k]
 answer = 0
 for i in range(n):
     s = set(arr[i:i+k])

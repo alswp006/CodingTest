646ce344c4dc6565e027a49b6c4fa7a040fb3d39
Minje Kim <102672547+alswp006@users.noreply.github.com>
2024-08-13 21:38:59 +0900

    [Silver II] Title: 222-풀링, Time: 656 ms, Memory: 77276 KB -BaekjoonHub

--- 백준/Silver/17829. 222－풀링/222－풀링.py
+++ 백준/Silver/17829. 222－풀링/222－풀링.py
@@ -1,17 +1,13 @@
 import sys
 
-input = sys.stdin.readline
-
 n = int(input())
-prev_arr = [list(map(int,input().split())) for _ in range(n)]
+prev_arr = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
 
 while n != 1:
     n //= 2
     arr = [[] for _ in range(n)]
     for i in range(n):
-        for j in range(n):
-            arr[i].append(sorted([prev_arr[i * 2][j * 2], prev_arr[i * 2 + 1][j * 2], prev_arr[i * 2][j * 2 + 1], prev_arr[i * 2 + 1][j * 2 + 1]])[2])
-
+        for j in range(n): arr[i].append(sorted([prev_arr[i * 2][j * 2], prev_arr[i * 2 + 1][j * 2], prev_arr[i * 2][j * 2 + 1], prev_arr[i * 2 + 1][j * 2 + 1]])[2])
     prev_arr = [[arr[temp_x][temp_y] for temp_y in range(n)] for temp_x in range(n)]
 
 print(prev_arr[0][0])

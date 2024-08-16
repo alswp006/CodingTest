5f6149be8f0d119442099bb761b14415c2fc24d7
Minje Kim <102672547+alswp006@users.noreply.github.com>
2024-08-16 22:17:00 +0900

    [Gold IV] Title: LCS 2, Time: 1636 ms, Memory: 423724 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.py
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -1,30 +1,29 @@
 s1 = input()
 s2 = input()
-arr = [[''] * len(s1) for _ in range(len(s2))]
+l_s1 = len(s1)
+l_s2 = len(s2)
+dp = [[''] * len(s1) for _ in range(len(s2))]
 
 flag = False
-for i in range(len(s1)):
+for i in range(l_s1):
     if s2[0] == s1[i]:
         flag=True
     if flag:
-        arr[0][i] = s2[0]
+        dp[0][i] = s2[0]
 
 flag = False
-for i in range(len(s2)):
+for i in range(l_s2):
     if s1[0] == s2[i]:
         flag = True
     if flag:
-        arr[i][0] = s1[0]
+        dp[i][0] = s1[0]
 
-for i in range(1, len(s2)):
-    for j in range(1, len(s1)):
+for i in range(1, l_s2):
+    for j in range(1, l_s1):
         if s2[i] == s1[j]:
-            arr[i][j] = arr[i - 1][j - 1] + s1[j]
-        else:
-            if len(arr[i - 1][j]) > len(arr[i][j - 1]):
-                arr[i][j] = arr[i - 1][j]
-            else:
-                arr[i][j] = arr[i][j - 1]
+            dp[i][j] = dp[i - 1][j - 1] + s1[j]
+            continue
+        dp[i][j] = dp[i - 1][j] if len(dp[i - 1][j]) > len(dp[i][j - 1]) else dp[i][j - 1]
 
-print(len(arr[-1][-1]))
-print(arr[-1][-1])
+print(len(dp[-1][-1]))
+print(dp[-1][-1])

acbad5c1eaf9e92197e87be528e5e51cf0cf702c
Minje Kim <102672547+alswp006@users.noreply.github.com>
2024-08-18 14:25:11 +0900

    [Silver III] Title: NBA 농구, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2852. NBA 농구/NBA 농구.py
+++ 백준/Silver/2852. NBA 농구/NBA 농구.py
@@ -1,36 +1,25 @@
 import sys
 
-input = sys.stdin.readline
+def cal_time(pres_time, time):
+    if point[0] > point[1]:
+        win_time[0] += time - pres_time
+    elif point[0] < point[1]:
+        win_time[1] += time - pres_time
+
 n = int(input())
+point = [0, 0]
+win_time = [0, 0]
+pres_time = 0
 
-team_time = {1: 0, 2: 0}
-goal = {1: 0, 2: 0}
-prev_team, prev_goal_time = map(str, input().split())
-goal[int(prev_team)] += 1
-prev_goal_time = int(prev_goal_time[:2]) * 60 + int(prev_goal_time[3:])
+for i in range(n):
+    team, time = map(str, sys.stdin.readline().split())
+    hour, minute = map(int, time.split(":"))
+    time = hour * 60 + minute
+    cal_time(pres_time, time)
+    point[int(team) - 1] += 1
+    pres_time = time
 
-for _ in range(n - 1):
-    pres_team, pres_goal_time = map(str, input().split())
-    pres_goal_time = int(pres_goal_time[:2]) * 60 + int(pres_goal_time[3:])
-    if goal[1] > goal[2]:
-        team_time[1] += pres_goal_time - prev_goal_time
-    elif goal[1] < goal[2]:
-        team_time[2] += pres_goal_time - prev_goal_time
-    goal[int(pres_team)] += 1
-    prev_team, prev_goal_time = pres_team, pres_goal_time
+cal_time(pres_time, 48*60)
 
-pres_goal_time = 48 * 60
-if goal[1] > goal[2]:
-    team_time[1] += pres_goal_time - prev_goal_time
-elif goal[1] < goal[2]:
-    team_time[2] += pres_goal_time - prev_goal_time
-
-team_one = str(team_time[1] // 60) if team_time[1] // 60 >= 10 else str(0) + str(team_time[1] // 60)
-team_one += ":"
-team_one += str(team_time[1] % 60) if team_time[1] % 60 >= 10 else str(0) + str(team_time[1] % 60)
-team_two = str(team_time[2] // 60) if team_time[2] // 60 >= 10 else str(0) + str(team_time[2] // 60)
-team_two += ":"
-team_two += str(team_time[2] % 60) if team_time[2] % 60 >= 10 else str(0) + str(team_time[2] % 60)
-
-print(team_one)
-print(team_two)
+print(f'{win_time[0] // 60:02}:{win_time[0] % 60:02}')
+print(f'{win_time[1] // 60:02}:{win_time[1] % 60:02}')

62c5161b41655cf4657780662dadd52996baa8c8
Minje Kim <102672547+alswp006@users.noreply.github.com>
2024-08-18 14:43:45 +0900

    [Silver III] Title: NBA 농구, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2852. NBA 농구/NBA 농구.py
+++ 백준/Silver/2852. NBA 농구/NBA 농구.py
@@ -1,17 +1,14 @@
-import sys
-
 n = int(input())
-point = [0, 0]
-win_time = [0, 0]
-pres_time = 0
+p, w_t = [0, 0], [0, 0]
+p_t = 0
 
 for i in range(n):
-    team, time_str = map(str, sys.stdin.readline().split())
-    time = int(time_str[:2]) * 60 + int(time_str[3:])
-    win_time[point[0] < point[1]] += time - pres_time if point[0] != point[1] else 0
-    point[int(team) - 1] += 1
-    pres_time = time
-win_time[point[0] < point[1]] += 48 * 60 - pres_time if point[0] != point[1] else 0
+    team, t_s = map(str, input().split())
+    time = int(t_s[:2]) * 60 + int(t_s[3:])
+    w_t[p[0] < p[1]] += time - p_t if p[0] != p[1] else 0
+    p[int(team) - 1] += 1
+    p_t = time
+w_t[p[0] < p[1]] += 48 * 60 - p_t if p[0] != p[1] else 0
 
-print(f'{win_time[0] // 60:02}:{win_time[0] % 60:02}')
-print(f'{win_time[1] // 60:02}:{win_time[1] % 60:02}')
+print(f'{w_t[0] // 60:02}:{w_t[0] % 60:02}')
+print(f'{w_t[1] // 60:02}:{w_t[1] % 60:02}')

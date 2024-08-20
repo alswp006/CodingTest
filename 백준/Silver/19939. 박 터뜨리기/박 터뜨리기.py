04b96bd2528e03984fc92a6c6733f915b6b74b5e
Minje Kim <102672547+alswp006@users.noreply.github.com>
2024-08-21 00:05:12 +0900

    [Silver IV] Title: 박 터뜨리기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/19939. 박 터뜨리기/박 터뜨리기.py
+++ 백준/Silver/19939. 박 터뜨리기/박 터뜨리기.py
@@ -1,5 +1,4 @@
 n, k = map(int,input().split())
-n -= (1 + k) * k // 2
+n -= ((1 + k) * k // 2)
 if n < 0: print(-1)
-elif n % k <= 0: print(k - 1)
-else: print(k)
+else: print(k - 1 if n % k == 0 else k)

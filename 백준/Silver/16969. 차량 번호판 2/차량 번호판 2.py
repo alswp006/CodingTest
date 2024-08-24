fafb5d7cd9120afeb2b91159c3b5ebc3c226d16f
Minje Kim <102672547+alswp006@users.noreply.github.com>
2024-08-25 00:03:10 +0900

    [Silver V] Title: 차량 번호판 2, Time: 136 ms, Memory: 114324 KB -BaekjoonHub

--- 백준/Silver/16969. 차량 번호판 2/차량 번호판 2.py
+++ 백준/Silver/16969. 차량 번호판 2/차량 번호판 2.py
@@ -3,7 +3,7 @@ temp = s[0]
 d = {'c':26, 'd':10}
 answer = d[temp]
 for i in s[1:]:
-    answer *= d[i]-1 if i == temp else d[i]
+    answer *= d[i] - 1 if i == temp else d[i]
     answer %= 1000000009
     temp = i
 print(answer)

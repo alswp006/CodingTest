7a29b68094d4b086682d19e1a3377b1e2c597f30
Minje Kim <alswp006@gmail.com>
2024-09-30 19:38:29 +0900

    [Gold V] Title: A와 B, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/12904. A와 B/A와 B.py
+++ 백준/Gold/12904. A와 B/A와 B.py
@@ -1,9 +1,7 @@
 s1 = input()
 s2 = list(input())
-num = len(s2)
-while len(s1) < num:
+while len(s1) < len(s2):
     temp = s2.pop()
     if temp == "B":
-        s2.reverse()
-    num -= 1
+        s2 = s2[::-1]
 print(int(s1 == ''.join(s2)))

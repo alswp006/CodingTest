f6991abdfeea760afd4469f407e6b53c45244606
Minje Kim <alswp006@gmail.com>
2024-10-04 17:56:10 +0900

    [Gold V] Title: 아우으 우아으이야!!, Time: 124 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/15922. 아우으 우아으이야！！/아우으 우아으이야！！.py
+++ 백준/Gold/15922. 아우으 우아으이야！！/아우으 우아으이야！！.py
@@ -8,6 +8,6 @@ for _ in range(n - 1):
     x, y = map(int, sys.stdin.readline().split())
     if right < x:
         empty += x - right
-    right = max(y, right)
+    right = y if y > right else right
 
 print(right - left - empty)

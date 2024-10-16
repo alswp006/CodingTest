49044df60fe199aba02c86c1a3952913477734c2
Minje Kim <alswp006@gmail.com>
2024-10-16 17:26:59 +0900

    [Silver II] Title: DFS와 BFS, Time: 160 ms, Memory: 17728 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,72 +1,71 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
+import java.util.stream.IntStream;
 
 public class Main {
-    private static int n;
-    private static ArrayList<Integer>[] li;
-    private static boolean[] visited;
-    private static StringBuilder sb = new StringBuilder();
+    static BufferedWriter bw;
+    static List<Integer>[] li;
+    static boolean[] visited;
+    static StringBuilder sb;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int v = Integer.parseInt(st.nextToken());
-        li = new ArrayList[n + 1];
-        for (int i = 0; i < n + 1; i++) {
-            li[i] = new ArrayList<Integer>();
-        }
+    public static void dfs(int v){
+        if (visited[v]) return;
 
-        for (int i = 0; i < m; i++){
-            st = new StringTokenizer(br.readLine());
-            int node = Integer.parseInt(st.nextToken());
-            int fin_node = Integer.parseInt(st.nextToken());
-            li[node].add(fin_node   );
-            li[fin_node].add(node);
-        }
-
-        for (int i = 0; i <= n; i++){
-            Collections.sort(li[i]);
-        }
-
-        visited = new boolean[n + 1];
-        dfs(v);
-        sb.append("\n");
-        visited = new boolean[n + 1];
-        bfs(v);
-
-        System.out.println(sb);
-    }
-
-    private static void dfs(int v){
         visited[v] = true;
         sb.append(v).append(" ");
-
-        for (int nextNode : li[v]){
-            if (!visited[nextNode]){
-                dfs(nextNode);
-            }
+        for (int i : li[v]){
+            dfs(i);
         }
     }
 
-    private static void bfs(int v){
-        visited[v] = true;
-        Queue<Integer> q = new LinkedList<>();
+    public static void bfs(int v){
+        Deque<Integer> q = new ArrayDeque<>();
         q.add(v);
 
         while (!q.isEmpty()){
-            int node = q.remove();
+            int node = q.removeFirst();
+            if (visited[node]) continue;
             sb.append(node).append(" ");
+            visited[node] = true;
 
-            for (int nextNode : li[node]){
-                if (!visited[nextNode]){
-                    visited[nextNode] = true;
-                    q.add(nextNode);
+            for (int i : li[node]){
+                if (!visited[i]){
+                    q.add(i);
                 }
             }
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+        li = new ArrayList[n + 1];
+        visited = new boolean[n + 1];
+
+        for (int i = 0; i < n + 1; i++){
+            li[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < m; i++){
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            li[x].add(y);
+            li[y].add(x);
+        }
+        for (int i = 0; i < n + 1; i++){
+            Collections.sort(li[i]);
+        }
+
+        dfs(v);
+        sb.append("\n");
+        visited = new boolean[n + 1];
+
+        bfs(v);
+        System.out.println(sb);
     }
 }

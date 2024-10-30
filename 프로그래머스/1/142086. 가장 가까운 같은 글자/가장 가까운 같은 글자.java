daef2e933605c379e440da25e62addff52bcb9da
Minje Kim <alswp006@gmail.com>
2024-10-30 23:13:08 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 5.39 ms, Memory: 82.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+        Map<Character, Integer> m = new HashMap<>();
+        
+        for (int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            
+            if (m.containsKey(c)){
+                answer[i] = i - m.get(c);
+                m.replace(c, i);
+            } else{
+                answer[i] = -1;
+                m.put(c, i);
+            }
+        }
+        return answer;
+    }
+}

a147c13d25c346ee763777f2e34bc9f0a25e03c4
Minje Kim <alswp006@gmail.com>
2024-10-31 13:30:59 +0900

    [level 1] Title: [1차] 비밀지도, Time: 8.66 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        
+        for (int i = 0; i < n; i++){
+            String s1 = String.format("%" + n + "s", Integer.toBinaryString(arr1[i])).replace(" ","0");
+            String s2 = String.format("%" + n + "s", Integer.toBinaryString(arr2[i])).replace(" ","0");
+            StringBuilder sb = new StringBuilder();
+            
+            for (int j = 0; j < n; j++){
+                if (s1.charAt(j) == '1' || s2.charAt(j) == '1') sb.append("#");
+                else sb.append(" ");
+            }
+            answer[i] = sb.toString();
+            
+        }
+        return answer;
+    }
+}

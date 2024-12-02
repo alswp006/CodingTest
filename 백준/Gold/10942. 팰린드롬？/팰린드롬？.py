76cd67da45ee2106ef7373160366594c72643319
Minje Kim <alswp006@gmail.com>
2024-12-02 11:26:06 +0900

    [Gold IV] Title: 팰린드롬?, Time: 2184 ms, Memory: 61948 KB -BaekjoonHub

---
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
@@ -0,0 +1,32 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+nums = list(map(int, input().split()))
+
+arr = [[0] * n for _ in range(n)]
+arr[0][0] = 1
+
+for i in range(1, n):
+    for j in range(i + 1):
+        if i - j < 0 or i + j >= n:
+            break
+        if nums[i - j] != nums[i + j]:
+            break
+        arr[i - j][i + j] = 1
+
+    if nums[i - 1] == nums[i]:
+        arr[i - 1][i] = 1
+
+    for j in range(i + 1):
+        if i - j < 0 or i + j + 1 >= n:
+            break
+        if nums[i - j] != nums[i + j + 1]:
+            break
+        arr[i - j][i + j + 1] = 1
+
+for i in range(int(input())):
+    a, b = map(int, input().split())
+    a, b = a - 1, b - 1
+    print(arr[a][b])
